4677914313b0e23201603d5c620340bc4b0111de
Raffi Keshishian <raffaelkeshishian8@gmail.com>
2023-10-30 11:44:06 +0200

    home complete

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,4 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
   constructor() {}
+
+  // carousel array
+  prolangs = [
+    {
+      image: 'beginners.jpg',
+      title: 'JavaScript & TypeScript',
+      description:
+        'JavaScript makes your webpage dynamic and more interactive and TypeScript has almost the same features as JavaScript but more specific on data types.',
+    },
+    {
+      image: 'intermediate.jpg',
+      title: 'SQL & PHP',
+      description:
+        'Structured Query Language(SQL) implements your database using CRUD operations. Hypertext PreProcessor(PHP) is a server side language used to create dynamic websites. It is also used to bind database with your website, alongside MySQL as part of an "XAMPP" configuration.',
+    },
+    {
+      image: 'advanced.jpg',
+      title: 'Angular & React',
+      description:
+        'Angular is a Javascript framework built using Typescript used to build complex enterprise-grade apps like single-page apps and progressive web apps, while React is a Javascript library and built using JSX used to build UI components in any app with frequently variable data.',
+    },
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  prolangs: any;
 
+  constructor(private data: DataService) {}
+
+  ngOnInit(): void {
+    this.prolangs = this.data.prolangs;
+  }
 }
